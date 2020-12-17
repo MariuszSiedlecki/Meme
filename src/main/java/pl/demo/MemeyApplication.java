724a0b4ab203c78b33a83d56a5d80d3707725257
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2020-12-17 01:25:57 +0100

    first commit

---
+++ src/main/java/pl/demo/MemeyApplication.java
@@ -0,0 +1,13 @@
+package pl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeyApplication.class, args);
+    }
+
+}
