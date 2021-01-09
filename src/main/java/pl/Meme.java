5008bad2b01a157f4465b33fc809de040e0f5b2c
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-01-09 20:07:54 +0100

    start

---
+++ src/main/java/pl/Meme.java
@@ -0,0 +1,20 @@
+package pl;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Meme {
+
+    private int id;
+    private String name;
+    @ToString.Exclude
+    private String imageUrl;
+    private String description;
+    private boolean favorite;
+
+}
