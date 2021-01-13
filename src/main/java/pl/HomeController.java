30685eba1a8a844697b70cc08184245f09f506f5
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-01-14 00:34:49 +0100

    controller

--- src/main/java/pl/HomeController.java
+++ src/main/java/pl/HomeController.java
@@ -1,9 +1,37 @@
 package pl;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+
+    List<Meme> memeList = new ArrayList<>();
+
+    @GetMapping("/")
+    public String main() {
+        return "home";
+    }
+
+    @PostMapping("/add")
+    public String add(@RequestParam String name,
+                      @RequestParam String imageUrl) {
+        Meme meme = new Meme(name, imageUrl);
+        memeList.add(meme);
+        return "home";
+    }
+
+    @GetMapping("/showAll")
+    public String showAll(ModelMap map) {
+        map.put("list", memeList);
+        return "result";
+    }
 
 }
