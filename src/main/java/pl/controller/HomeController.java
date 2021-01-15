731e6c7cf703b5c2cf4997b8cd51d280aca5e2f6
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-01-16 00:21:24 +0100

    changes3

--- src/main/java/pl/controller/HomeController.java
+++ src/main/java/pl/controller/HomeController.java
@@ -1,10 +1,11 @@
-package pl;
+package pl.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.model.Meme;
 
 import java.util.ArrayList;
 import java.util.List;
