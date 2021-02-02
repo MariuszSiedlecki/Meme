e3e6b40059620f1fd14e185ae49ff57d71a1662a
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-02-03 00:19:15 +0100

    schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS TAB_EMPLOEEY;
+
+CREATE TABLE TAB_EMPLOEEY(
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            first_name VARCHAR(255) NOT NULL ,
+            last_name VARCHAR(255) NOT NULL ,
+            email VARCHAR(255) NOT NULL,
+);
