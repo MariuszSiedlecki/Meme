502f8cf3b4cee49e69265760ace2bb76093e5c74
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-02-04 14:10:03 +0100

    schema.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TAB_EMPLOEEY(first_name,last_name,email)VALUES
+('Jan','Nowak','nowak@wp.pl'),
+('Ola','Nowak','ola@wp.pl'),
+('Piotr','Kowalski','kowal@onet.pl'),
+('Grzegorz','Popaprany','popap@gmail.com.pl'),
+('Maksiu','Siedlecki','max@gmail.com.pl');
