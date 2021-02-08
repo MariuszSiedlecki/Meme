6a5d73a089c88e1b5af93e2db9b293fb85a31483
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-02-08 13:03:07 +0100

    Spring Security

---
+++ src/main/java/pl/controller/security/UserDto.java
@@ -0,0 +1,35 @@
+package pl.controller.security;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class UserDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String lastName;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    @Email
+    private String email;
+
+    @ElementCollection
+    private Set<String>roles;
+}
