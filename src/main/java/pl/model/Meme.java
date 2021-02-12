293aefde8e52c9c893ac67d9623ac25327c97a28
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-02-13 00:26:01 +0100

    DateTimerFormater

--- src/main/java/pl/model/Meme.java
+++ src/main/java/pl/model/Meme.java
@@ -1,20 +1,32 @@
 package pl.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.hibernate.validator.constraints.URL;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.util.StreamUtils;
 
+import javax.persistence.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+
+@Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class Meme {
 
     private int id;
+    @NonNull
     private String name;
+    @URL
+    @NonNull
     @ToString.Exclude
     private String imageUrl;
+    @NonNull
+    @Column(columnDefinition = "LONGTEXT")
     private String description;
+    @NonNull
     private boolean favorite;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime updateDate = LocalDateTime.now();
