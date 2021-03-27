8bee9800cc906ca3481a82872958d38473d970ed
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2021-03-27 21:32:16 +0100

    Extra Springseciurity5 Thymeleaf

--- src/main/java/pl/controller/security/UserDto.java
+++ src/main/java/pl/controller/security/UserDto.java
@@ -4,15 +4,19 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pl.model.Meme;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class UserDto {
+public class UserDto implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +35,57 @@ public class UserDto {
     @Email
     private String email;
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     private Set<String>roles;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Integer>favoriteMemeIdList = new HashSet<>();
+
+    @OneToMany(mappedBy = "userDtoOwner", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @EqualsAndHashCode.Exclude
+    private Set<Meme>memeList = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getGrantedAuthorities(roles);
+    }
+    public List<GrantedAuthority> getGrantedAuthorities(Set<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for(String role :roles){
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+
+    public void addOrDeleteMemeToFavoriteList( int meme_id){
+        if(this.favoriteMemeIdList.contains(meme_id)){
+            this.favoriteMemeIdList.remove(meme_id);
+        }else {
+            this.favoriteMemeIdList.add(meme_id);
+        }
+    }
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
